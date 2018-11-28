a27c474783339872d1165bb62b7f9972677051b4
unknown <palakkeni@gmail.com>
2018-11-28 17:37:54 +0530

    Set App for Heroku-1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise=global.Promise
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://palak:abc123@ds119304.mlab.com:19304/node-todo-api-for-node-js-course'
+  mlab: 'mongodb://palakkeni5:palakkeni5@ds119304.mlab.com:19304/node-todo-api-for-node-js-course'
 };
 mongoose.connect( db.localhost ||  db.mlab,{ useNewUrlParser: true });
 //mongoose.connect("mongodb://localhost:27017/TodoApp",{ useNewUrlParser: true })
