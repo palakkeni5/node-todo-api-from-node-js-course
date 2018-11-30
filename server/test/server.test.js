246198a98e512baf72c8f7cfb971af5b4bcdfa36
unknown <palakkeni@gmail.com>
2018-11-30 16:45:12 +0530

    Test DELETE /todos/:id route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -104,3 +104,41 @@ describe('GET /todos/:id',()=>{
 
     })
 })
+
+describe("DELETE /todos/:id",()=>{
+    it('should remove a Todo', (done)=>{
+        var hexID = todos[1]._id.toHexString()
+
+        request(app)
+        .delete(`/todos/${hexID}`)
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo._id).toBe(hexID)
+        })
+        .end((err, res) =>{
+            if (err){
+                return done(err)
+            }
+            Todo.findById(hexID).then((todo)=>{
+                expect(todo).toNotExist()
+                done()
+            }).catch((e)=>{
+                console.log(e)
+            })
+        })
+    })
+    it('should return 404 if todo is not found', (done)=>{
+        var new_oid=new ObjectID()
+        request(app)
+        .delete(`/todos/${new_oid.toHexString()}`)
+        .expect(404)
+        .end(done)
+    })
+    it('should return 404 if todo ObjectID is invalid', (done)=>{
+        request(app)
+        .delete(`/todos/123`)
+        .expect(404)
+        .end(done)
+
+    })
+})
