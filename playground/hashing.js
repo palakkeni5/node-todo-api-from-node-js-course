0836699513a72080e7f3e8533f6219489f02beef
palakkeni5 <palakkeni@gmail.com>
2019-01-05 23:06:35 +0530

    Hash User Password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,29 @@
 const{SHA256}=require('crypto-js')
 const jwt =require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-var data={
-    id: 10
-}
+var password = '123abc!'
 
-var token = jwt.sign(data, '123abc')
-console.log(token)
+bcrypt.genSalt(10 , (err , salt)=>{
+    bcrypt.hash(password , salt , (err, hash)=>{
+        console.log(hash)
+    })
+})
 
-var decoded=jwt.verify(token, '123abc')
-console.log('decoded', decoded)
+var hashedPassword= '$2a$10$pzcby6agakbcLtDxh6ifsOcFMkKPgE1iV2xuRVfXVgREzSNHnuEvu'
+
+bcrypt.compare('123!', hashedPassword, (err,res)=>{
+    console.log(res)
+})
+// var data={
+//     id: 10
+// }
+
+// var token = jwt.sign(data, '123abc')
+// console.log(token)
+
+// var decoded=jwt.verify(token, '123abc')
+// console.log('decoded', decoded)
 //jwt.verify()
 // var message = "I am palakkeni"
 // var hash = SHA256(message).toString()
